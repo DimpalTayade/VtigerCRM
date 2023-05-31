30d922085646b4d9f4344da2b06949646896c887
DimpalTayade <132127545+DimpalTayade@users.noreply.github.com>
2023-05-31 09:47:52 +0530

    Add files via upload

---
+++ SelfInformation.java
@@ -0,0 +1,9 @@
+class SelfInformation
+{
+public static void main(String[] args)
+{
+System.out.println("Name:-Dimpal");
+System.out.println("College:-kbc nmu");
+System.out.println("email:-dimpaltayade21@gmail.com");
+}
+}
